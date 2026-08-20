d099ba262216360fd8480da164d3525d2950abe8
UniCornPrincess69 <115454083+UniCornPrincess69@users.noreply.github.com>
2026-08-20 11:16:08 +0200

    Beautification
    Did some beautification of the UI and Splashscreen

--- WorkTimeTracker/MainPage.xaml.cs
+++ WorkTimeTracker/MainPage.xaml.cs
@@ -11,25 +11,38 @@ namespace WorkTimeTracker
 
         private readonly SaveSystem saveSystem;
         private WorkTimeData? currentWorkData;
+        private IDispatcherTimer? elapsedTimer;
 
         private readonly string startButtonName = "StartBtn";
         private readonly string stopButtonName = "StopBtn";
         private readonly string overviewButtonName = "OverviewBtn";
+        private readonly string statusWork = "Beschäftigt";
+        private readonly string statusIdle = "Nicht beschäftigt";
+        private readonly string timerReset = "00:00:00";
 
         private static readonly Color startColor = Colors.Green;
         private static readonly Color stopColor = Colors.Red;
         private static readonly Color overviewColor = Colors.DeepSkyBlue;
         private static readonly Color inactiveColor = Colors.DarkGray;
 
+        private WorkStatus currentStatus =>
+            currentWorkData is not null && currentWorkData.EndTime is null
+            ? WorkStatus.Working
+            : WorkStatus.NotWorking;
         #endregion
 
         public MainPage(SaveSystem saveSystem)
         {
             InitializeComponent();
+            elapsedTimer = Dispatcher.CreateTimer();
+            elapsedTimer.Interval = TimeSpan.FromSeconds(1);
+            elapsedTimer.Tick += OnElapsedTimerTick;
             this.saveSystem = saveSystem;
             currentWorkData = saveSystem.Load();
             ButtonInitialization();
+            UpdateStatus();
             saveSystem.DataChanged += OnDataChanged;
+            if(currentStatus == WorkStatus.Working) elapsedTimer?.Start();
         }
 
         private void OnStartClicked(object? sender, EventArgs e)
@@ -39,10 +52,12 @@ namespace WorkTimeTracker
             {
                 startButton.IsEnabled = false; // Disable the Start button
                 startButton.BackgroundColor = inactiveColor;
-                currentWorkData = currentWorkData ?? new WorkTimeData { StartTime = GetCurrentTime() };
+                currentWorkData = new WorkTimeData { StartTime = GetCurrentTime() };
                 saveSystem.Save(currentWorkData);
+                elapsedTimer?.Start();
                 stopButton.IsEnabled = true; // Enable the Stop button
                 stopButton.BackgroundColor = stopColor;
+                UpdateStatus();
                 //Debug.WriteLine($"stopColor: {stopColor}");
                 //Debug.WriteLine($"buttonColor: {stopButton.BackgroundColor}");
             }
@@ -50,24 +65,27 @@ namespace WorkTimeTracker
 
         private void OnStopClicked(object? sender, EventArgs e)
         {
-           //Debug.WriteLine("Stop button clicked");
+            //Debug.WriteLine("Stop button clicked");
             if (sender != null)
             {
                 stopButton.IsEnabled = false; // Disable the Stop button
                 stopButton.BackgroundColor = inactiveColor;
                 currentWorkData.EndTime = GetCurrentTime();
                 saveSystem.Save(currentWorkData);
+                elapsedTimer?.Stop();
+                ElapsedTimeLabel.Text = timerReset;
                 currentWorkData = null;
                 startButton.IsEnabled = true; // Enable the Start button
                 startButton.BackgroundColor = startColor;
                 overviewButton.IsEnabled = true;
                 overviewButton.BackgroundColor = overviewColor;
+                UpdateStatus();
             }
         }
 
         private async void OnOverviewClicked(object? sender, EventArgs e)
         {
-            
+
             await Shell.Current.GoToAsync(nameof(OverviewPage));
         }
 
@@ -85,20 +103,20 @@ namespace WorkTimeTracker
             {
                 startButton.IsEnabled = false;
                 startButton.BackgroundColor = inactiveColor;
-                stopButton.IsEnabled= true;
+                stopButton.IsEnabled = true;
                 stopButton.BackgroundColor = stopColor;
             }
             if (startButton.IsEnabled)
             {
-                stopButton.IsEnabled= false;
+                stopButton.IsEnabled = false;
                 stopButton.BackgroundColor = inactiveColor;
             }
-            if (saveSystem.GetTrackedData().Count == 0) 
-            { 
+            if (saveSystem.GetTrackedData().Count == 0)
+            {
                 overviewButton.IsEnabled = false;
                 overviewButton.BackgroundColor = inactiveColor;
             }
-            
+
         }
 
 
@@ -112,7 +130,7 @@ namespace WorkTimeTracker
                 now.Day,
                 now.Hour,
                 now.Minute,
-                0);
+                now.Second);
         }
 
         private void OnDataChanged(object? sender, EventArgs e)
@@ -120,5 +138,31 @@ namespace WorkTimeTracker
             overviewButton.IsEnabled = true;
             overviewButton.BackgroundColor = overviewColor;
         }
+
+        private void UpdateStatus()
+        {
+            switch (currentStatus)
+            {
+                case WorkStatus.Working:
+                    StatusLabel.Text = statusWork;
+                    break;
+
+                case WorkStatus.NotWorking:
+                    StatusLabel.Text = statusIdle;
+                    break;
+            }
+        }
+
+        private void OnElapsedTimerTick(object? sender, EventArgs e)
+        {
+            if (currentWorkData is null) return;
+
+            TimeSpan elapsed = GetCurrentTime() - currentWorkData.StartTime;
+
+            ElapsedTimeLabel.Text = elapsed.ToString(@"hh\:mm\:ss");
+
+            Debug.WriteLine(elapsed.ToString());
+        }
+
     }
 }
--- WorkTimeTracker/WorkTimeData.cs
+++ WorkTimeTracker/WorkTimeData.cs
@@ -4,14 +4,19 @@ using System.Text;
 
 namespace WorkTimeTracker
 {
+    public enum WorkStatus
+    {
+        NotWorking,
+        Working
+    }
+  
     public class WorkTimeData()
     {
-        
         public DateTime StartTime { get; set ; }
         public DateTime? EndTime {  get; set; }
-
-
+        public WorkStatus Status { get; set; }
         public TimeSpan? TimeSpan => EndTime.HasValue ? EndTime.Value - StartTime : null;
+
                
     }
 }
